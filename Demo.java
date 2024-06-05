31b143039ba56831d5ff9519b549b1eab17eab2a
Bonolo Solomon <bonolosolomon@Bonolos-MacBook-Air.local>
2024-06-05 09:13:19 +0200

    First commit

---
+++ Demo.java
@@ -0,0 +1,25 @@
+class Student {
+    int rollno;
+    String name;
+    int marks;
+}
+
+/**
+ * InnerStudent
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+
+     int num1 = 5;
+     int num2 = 5;
+
+     int results = (num1 * num2);
+
+     System.out.println(results);
+
+
+
+    }
+    
+}
---
+++ project1/Demo2.java
@@ -0,0 +1,35 @@
+package project1;
+
+class user { 
+    String name;
+    int id;
+    String description;
+    
+}
+
+public class Demo2 {
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,3,4,5,6};
+
+        for(int i = 0;i < numbers.length;i++) {
+            System.out.println(numbers[i]);
+        }
+        // user user1 = new user();
+        // user1.id = 1;
+        // user1.name = "Avery";
+        // user1.description = "My name is Avery";
+
+        // user user2 = new user();
+        // user2.id = 2;
+        // user2.name = "John";
+        // user2.description = "My name is John";
+
+        // user user3 = new user();
+        // user3.id = 2;
+        // user3.name = "Jack";
+        // user3.description = "My name is Jack";
+        // System.out.println(user1.name);
+    }
+}
+
